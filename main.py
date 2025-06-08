8a9e2179b009783280f78ad066105575a0712649
D4VVID0S <matix09@live.it>
2025-06-08 22:23:41 +0200

    adds limit to 2 decimal places for float values

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ main.py
 Entry point for the Python Pokédex application.
 """
 
-from store_data.pokemons_dataframe import load_pokemons_dataframe
+from store_data.pokemons_dataframe import load_pokemons_to_dataframe
+from store_data.pokemons_dataframe import edit_stats_to_basic_uom
+import polars as pl
 
 
 def main():
     """
     Loads all Pokémon into a DataFrame and displays a summary.
     """
-    df = load_pokemons_dataframe()
+    # set display precision to 2 decimals for all Float32/Float64
+    pl.Config.set_float_precision(2)
+    
+    df = load_pokemons_to_dataframe()
+    df = edit_stats_to_basic_uom(df)
     print(f"Total Pokémon fetched: {len(df)}")
     print(df.head(10))
 
