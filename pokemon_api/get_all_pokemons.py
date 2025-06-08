c833597032bb02c94ac2c06d18584eb16c6740df
D4VVID0S <matix09@live.it>
2025-06-08 21:59:51 +0200

    gets pokemon stats from the api

---
+++ pokemon_api/get_all_pokemons.py
@@ -0,0 +1,69 @@
+# Provides a function to retrieve all Pokémon entries
+# (name and URL) from the PokéAPI, handling pagination.
+
+import os
+import requests
+from dotenv import load_dotenv
+import time
+
+API_BASE_URL = "https://pokeapi.co/api/v2/"
+
+def get_all_pokemons():
+    """
+    Retrieve all Pokémon entries (name and URL) from the PokéAPI.
+
+    This function iterates through all paginated results, collecting
+    each page's 'results' list until there are no more pages.
+
+    Returns:
+        A list of dictionaries for each Pokémon, containing:
+        'id' (int)
+        'name' (str)
+        'height' (int)
+        'weight' (int)
+        'abilities' (list of str)
+        'sprites' (dict)
+        'stats' (list of {'stat': str, 'base_stat': int})
+        'types' (list of str)
+    
+    References:
+        - PokéAPI Pokémon endpoint: https://pokeapi.co/docs/v2#pokemon
+        - requests documentation: https://docs.python-requests.org/
+    """
+
+    pokemons = []
+    next_url = f"{API_BASE_URL}/pokemon"
+
+    while next_url:
+        # Seng GET request to the current page URL
+        response = requests.get(next_url)
+        response.raise_for_status()
+        data = response.json()
+
+        # Append this page's results (list of {'name', 'url'} dicts)
+        pokemons.extend(data.get('results', []))
+
+        # Update next_url to the 'next' page URL, or None if done
+        next_url = data.get('next')
+
+        # Fetch full details for each Pokemon
+        detailed_pokemons = []
+        for entry in pokemons:
+            resp = requests.get(entry['url'])
+            resp.raise_for_status()
+            data = resp.json()
+            detailed_pokemons.append({
+                "id": data["id"],
+                "name": data["name"],
+                "height": data["height"],
+                "weight": data["weight"],
+                "abilities": [ability_entry["ability"]["name"] for ability_entry in data["abilities"]],
+                "sprites": data["sprites"],
+                "stats": [
+                    {"stat": stat_entry["stat"]["name"], "base_stat": stat_entry["base_stat"]}
+                    for stat_entry in data["stats"]
+                ],
+                "types": [type_entry["type"]["name"] for type_entry in data["types"]],
+            })
+            time.sleep(0.1)
+        return detailed_pokemons
